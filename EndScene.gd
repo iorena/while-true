40f7c2420f901510f9393be4f580c41d0960eb3e
Essi Salmenkivi <essi@skivi.net>
2020-10-06 00:44:13 +0300

    add TV text to end scene

--- EndScene.gd
+++ EndScene.gd
@@ -8,6 +8,7 @@ onready var bp = get_node("BottomPanel")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	unlock_player_actions()
+	bp.log_clear()
 	player.facing_direction = player.FACING_DOWN
 	player.play_idle_animation()
 	set_new_player_actions(Actions.TURN_RIGHT, Actions.TURN_RIGHT, Actions.TURN_RIGHT, Actions.TURN_RIGHT)
@@ -23,3 +24,15 @@ func unlock_player_actions():
 	player.can_act = true
 	sp.hide_locks()
 	sp.set_action_index_and_clear_rest(0)
+
+
+func _on_EndTimer_timeout():
+	player.camera.set_smooth_zoom(0.1)
+	for i in range(40):
+		$Map/Darkness.color.a += 0.05
+		yield(get_tree().create_timer(0.1), "timeout")
+	queue_free()
+
+
+func _on_TVTimer_timeout():
+	$BottomPanel.log_write("TV ANNOUNCER: A horrific explosion happened at Roboware Industries this morning. The police are investigating the event.")
